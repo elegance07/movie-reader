f05153d679d1ed1da2e9cf809165f62afdcfca3a
elegance07 <59148349+elegance07@users.noreply.github.com>
2022-07-25 14:45:06 +0300

    Add files via upload

---
+++ engine/data/mws-film.conf.php
@@ -0,0 +1,155 @@
+<?PHP 
+
+//MWS Film Reader Configuration
+
+$mws_film = array (
+
+'achange' => "1",
+
+'api_key' => "",
+
+'clean_text' => "a:4:{i:0;s:6:'quotes';i:1;s:6:'spaces';i:2;s:10:'breaklines';i:3;s:10:'hyperlinks';}",
+
+'clist' => "a:23:{i:0;s:4:'name';i:1;s:4:'year';i:2;s:3:'url';i:3;s:8:'director';i:4;s:9:'screenman';i:5;s:7:'writers';i:6;s:6:'actors';i:7;s:5:'story';i:8;s:6:'genres';i:9;s:7:'country';i:10;s:8:'language';i:11;s:6:'season';i:12;s:7:'episode';i:13;s:7:'runtime';i:14;s:9:'datelocal';i:15;s:10:'created_by';i:16;s:6:'budget';i:17;s:7:'revenue';i:18;s:7:'tagline';i:19;s:9:'locations';i:20;s:6:'author';i:21;s:14:'productionfirm';i:22;s:9:'namelocal';}",
+
+'default_cover' => "",
+
+'downcover' => "1",
+
+'film_actors' => "stars",
+
+'film_author' => "author",
+
+'film_budget' => "budget",
+
+'film_country' => "country",
+
+'film_created_by' => "created_by",
+
+'film_datelocal' => "",
+
+'film_director' => "director",
+
+'film_episode' => "episode",
+
+'film_episodes' => "episodes",
+
+'film_genres' => "genre",
+
+'film_img' => "cover",
+
+'film_language' => "language",
+
+'film_locations' => "country",
+
+'film_name' => "_title_",
+
+'film_namelocal' => "namelocal",
+
+'film_productionfirm' => "productionfirm",
+
+'film_ratinga' => "rating",
+
+'film_ratingb' => "",
+
+'film_ratingc' => "votes",
+
+'film_revenue' => "revenue",
+
+'film_runtime' => "time",
+
+'film_screenman' => "screenman",
+
+'film_screens' => "screens",
+
+'film_season' => "season",
+
+'film_season_count' => "season_count",
+
+'film_seasons' => "seasons",
+
+'film_story' => "_short_",
+
+'film_tagline' => "slogan",
+
+'film_trailer' => "trailer",
+
+'film_trailer_mobil' => "trailer_mobile",
+
+'film_type' => "type",
+
+'film_url' => "url",
+
+'film_writers' => "writers",
+
+'film_year' => "year",
+
+'film_years' => "years",
+
+'imgsize' => "1",
+
+'insertwm' => "0",
+
+'orderw' => "0",
+
+'overwrite' => "1",
+
+'overwriteimg' => "1",
+
+'p_1f9da1c54f01ff88ffb120b527c46e6a' => "1",
+
+'p_2985aa9558e0694bd2cd1c38e0da4651' => "1",
+
+'p_8d0439e6024c004f5edbc1487a554460' => "1",
+
+'rating_sep' => "0",
+
+'read_trailer' => "1",
+
+'req_achange' => "0",
+
+'req_add_as' => "0",
+
+'req_del_post' => "0",
+
+'req_multi' => "0",
+
+'req_multi_dlimit' => "0",
+
+'req_multi_rlimit' => "0",
+
+'req_multic' => "0",
+
+'req_notify' => "0",
+
+'req_notifyall' => "0",
+
+'req_same' => "0",
+
+'resizeimg' => "1",
+
+'resizesize' => "400",
+
+'resizetype' => "0",
+
+'rtext' => "Crime=>Crime\r\nDrame=>Drame\r\nThriller=>Thriller",
+
+'screens_count' => "0",
+
+'screens_down' => "0",
+
+'tags' => "a:15:{s:4:'name';s:6:'xfield';s:4:'year';s:6:'xfield';s:3:'url';s:6:'xfield';s:8:'director';s:6:'xfield';s:9:'screenman';s:6:'xfield';s:7:'writers';s:6:'xfield';s:6:'actors';s:6:'xfield';s:5:'story';s:6:'xfield';s:6:'genres';s:6:'xfield';s:7:'country';s:6:'xfield';s:8:'language';s:6:'xfield';s:7:'runtime';s:6:'xfield';s:9:'datelocal';s:6:'xfield';s:6:'author';s:6:'xfield';s:9:'namelocal';s:6:'xfield';}",
+
+'tclist' => "a:23:{i:0;s:4:'name';i:1;s:4:'year';i:2;s:3:'url';i:3;s:8:'director';i:4;s:9:'screenman';i:5;s:7:'writers';i:6;s:6:'actors';i:7;s:5:'story';i:8;s:6:'genres';i:9;s:7:'country';i:10;s:8:'language';i:11;s:6:'season';i:12;s:7:'episode';i:13;s:7:'runtime';i:14;s:9:'datelocal';i:15;s:10:'created_by';i:16;s:6:'budget';i:17;s:7:'revenue';i:18;s:7:'tagline';i:19;s:9:'locations';i:20;s:6:'author';i:21;s:14:'productionfirm';i:22;s:9:'namelocal';}",
+
+'tnames' => "a:15:{s:4:'name';s:4:'name';s:4:'year';s:4:'year';s:3:'url';s:3:'url';s:8:'director';s:8:'director';s:9:'screenman';s:9:'screenman';s:7:'writers';s:7:'writers';s:6:'actors';s:6:'actors';s:5:'story';s:5:'story';s:6:'genres';s:5:'genre';s:7:'country';s:7:'country';s:8:'language';s:4:'lang';s:7:'runtime';s:7:'runtime';s:9:'datelocal';s:9:'datelocal';s:6:'author';s:6:'author';s:9:'namelocal';s:9:'namelocal';}",
+
+'trailer_max' => "10",
+
+'trailer_term' => "Bande annonce",
+
+'trailertag' => "long",
+
+);
+
+?>
